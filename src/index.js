7d56f8ea41e1e15a1fbb9d6bedf7bdf8f9f142a7
umbertojr85 <umbertojr85@gmail.com>
2023-02-12 23:14:37 -0800

    The App Component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import 'font-awesome/css/font-awesome.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
