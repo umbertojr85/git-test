751b1ac423d17523b867e3f72c814a4c2d02fe0d
umbertojr85 <umbertojr85@gmail.com>
2023-02-18 19:03:56 -0800

    campsitesSlice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+import CampsitesList from './features/campsites/CampsitesList.js';
 import './App.css';
+import { CAMPSITES } from './app/shared/CAMPSITES';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='App'>
-      <Navbar dark color='primary' sticky='top' expand='md'>
-        <Container>
-          <NavbarBrand href='/'>
-            <img src={NucampLogo} alt='nucamp logo' />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
-      I'm ready for workshop!
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
